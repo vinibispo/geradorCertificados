e4c92cf43bd99504603101ab9161d5569d124c61
vinibispo <vini.bispo015@gmail.com>
2020-02-16 15:45:58 -0300

    we need to fix this

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from PIL import Image, ImageFont, ImageDraw
 def gera_certificado(nome_pessoa):
-    certificado = 'certificado.png'
-    certificado_pessoa = certificado + nome_pessoa
+    certificado = 'certificado'
+    certificado_pessoa = certificado + nome_pessoa + ".png"
+    certificado += ".png"
     imagem_certificado = Image.open(certificado)
     imagem_certificado_desenho = ImageDraw.Draw(imagem_certificado)
-    caminho_imagem = "./fonts/SourceSerifPro-Bold.ttf"
+    caminho_imagem = "fonts/SourceSerifPro-Bold.ttf"
     fonte_certificado = ImageFont.truetype(caminho_imagem, 34, encoding="unic")
     imagem_certificado_desenho.text((22, 243), nome_pessoa, font=fonte_certificado, fill=(115,111,72))
-    imagem_certificado.save("./images/" +certificado_pessoa, "PNG", resolution=100.0)
+    imagem_certificado.save("images/" +certificado_pessoa, "PNG", resolution=100.0)
     return certificado_pessoa
+print(gera_certificado("Adolfo"))
