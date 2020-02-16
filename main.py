bec1a8708eb9b09d6323a5e446312b0508f590eb
vinibispo <vini.bispo015@gmail.com>
2020-02-16 16:20:38 -0300

    it's working certer the text

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ def gera_certificado(nome_completo_pessoa):
     certificado_pessoa = certificado +nome_pessoa+ ".png"
     certificado += ".png"
     imagem_certificado = Image.open(certificado)
-    print(imagem_certificado.size)
     imagem_certificado_desenho = ImageDraw.Draw(imagem_certificado)
     caminho_imagem = "fonts/SourceSerifPro-Bold.ttf"
     fonte_certificado = ImageFont.truetype(caminho_imagem, 44, encoding="unic")
-    imagem_certificado_desenho.text((200, 243), nome_completo_pessoa.upper(), font=fonte_certificado, fill=(115,111,72))
+    text_size = imagem_certificado_desenho.textsize(nome_completo_pessoa, fonte_certificado)
+
+    imagem_certificado_desenho.text(((imagem_certificado.size[0] - text_size[0])/ 2, 243), nome_completo_pessoa.upper(), font=fonte_certificado, fill=(115,111,72))
     imagem_certificado.save("images/" +certificado_pessoa, "PNG", resolution=100.0)
     return certificado_pessoa
-print(gera_certificado("Mellany Aguiar"))
+print(gera_certificado())
