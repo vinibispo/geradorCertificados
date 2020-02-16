d50d2b3f8d4b0ad1fd8f3cbca200b19604b29f0a
vinibispo <vini.bispo015@gmail.com>
2020-02-16 15:36:54 -0300

    adding things and creating .gitkeep

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from PIL import Image, ImageFont, ImageDraw
 def gera_certificado(nome_pessoa):
     certificado = 'certificado.png'
+    certificado_pessoa = certificado + nome_pessoa
     imagem_certificado = Image.open(certificado)
     imagem_certificado_desenho = ImageDraw.Draw(imagem_certificado)
     caminho_imagem = "./fonts/SourceSerifPro-Bold.ttf"
-    fonte_certificado = ImageFont.truetype(caminho_imagem)
-    imagem_certificado_desenho.text((640, 1000), nome_pessoa, font=fonte_certificado, fill=(115,111,72))
+    fonte_certificado = ImageFont.truetype(caminho_imagem, 34, encoding="unic")
+    imagem_certificado_desenho.text((22, 243), nome_pessoa, font=fonte_certificado, fill=(115,111,72))
+    imagem_ce
 # gera_certificado()
