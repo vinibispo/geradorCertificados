0eb3fc79a25824641df0ff1523299d1f8981469a
vinibispo <vini.bispo015@gmail.com>
2020-02-16 15:48:49 -0300

    let's try again

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def gera_certificado(nome_pessoa):
     imagem_certificado = Image.open(certificado)
     imagem_certificado_desenho = ImageDraw.Draw(imagem_certificado)
     caminho_imagem = "fonts/SourceSerifPro-Bold.ttf"
-    fonte_certificado = ImageFont.truetype(caminho_imagem, 34, encoding="unic")
-    imagem_certificado_desenho.text((22, 243), nome_pessoa, font=fonte_certificado, fill=(115,111,72))
+    fonte_certificado = ImageFont.truetype(caminho_imagem, 44, encoding="unic")
+    imagem_certificado_desenho.text((34, 243), nome_pessoa.upper().center(20), font=fonte_certificado, fill=(115,111,72))
     imagem_certificado.save("images/" +certificado_pessoa, "PNG", resolution=100.0)
     return certificado_pessoa
 print(gera_certificado("Adolfo"))
